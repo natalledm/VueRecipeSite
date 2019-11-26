b647df3f3d0d09b380aac3e3450a5fcd473af847
Nat <natalledm@nat.note>
2019-11-26 12:15:19 +0100

    :construction:

---
+++ src/components/Carousel.vue
@@ -0,0 +1,15 @@
+<template>
+<div> hello Nat </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'Carousel'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="body">
+    <span class="carousel">
+      <Carousel />
+    </span>
+    <Lists />
+    <span class="test-button">
+      Test button and text.
+    </span>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+import Carousel from '@/components/Carousel.vue'
+import Lists from '@/components/Lists.vue'
+
+export default {
+  name: 'Home'
+  // components: {
+  //   HelloWorld
+  // }
+};
+</script>
